f7c9a3710d84b40917b4ff80d46d1f5886880777
roberthluo <robert.otherone@gmail.com>
2017-01-31 22:30:11 -0500

    Deques and Randomized Queues Assignment

---
+++ src/assignment2/Deque.java
@@ -0,0 +1,49 @@
+package assignment2;
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+
+	// construct an empty deque
+	public Deque()
+	{
+		
+	}
+	
+	// is the deque empty?
+	public boolean isEmpty()
+	{
+		return false;
+		
+	}
+	
+	// return the number of items on the deque
+	public void addFirst1(Item item)
+	{
+		
+	}
+	
+	// add the item to the front
+	public void AddFirst(Item item)
+	{
+		
+	}
+	
+	// remove and return the item from the front
+	public Item removeFirst()
+	{
+		return null;
+		
+	}
+	
+	// remove and return the item from the end
+	public Item removeLast()
+	{
+		return null;
+	}
+	
+	// return an Iterator over items in order from front to end
+	public Iterator<Item> iterator()
+	{
+		return null;
+	}
+}
