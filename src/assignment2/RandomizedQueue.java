2e7778b84e52d0609e165949db427b193d672ac9
roberthluo <robert.otherone@gmail.com>
2017-02-06 21:47:09 -0500

    Added RandomizedQueue class

---
+++ src/assignment2/RandomizedQueue.java
@@ -0,0 +1,43 @@
+package assignment2;
+
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+	
+	public RandomizedQueue()
+	{
+		
+	}
+	
+	public boolean IsEmpty()
+	{
+		return true;
+	}
+	
+	public int Size()
+	{
+		return 0;
+	}
+	
+	public void Enqueue(Item item)
+	{
+		
+	}
+	
+	public Item Dequeue()
+	{
+		return null;
+	}
+	
+	public Item Sample()
+	{
+		return null;
+	}
+
+	@Override
+	public Iterator<Item> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
